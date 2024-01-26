b8f2f67c5ed534638a84d52e7154557bc133add2
Taariq Elliott <taariq.elliott@yahoo.com>
2024-01-25 21:24:40 -0800

    first push to repo

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useRef, useState } from 'react'
+import './App.css'
+
+function App() {
+  const exportRef = useRef();
+
+  return (
+    <>
+      <div className="parent">
+        <div>
+          <p>
+            Test For Palette Picker & Exporter
+          </p>
+          <div ref={exportRef} className="colors">
+            <h1>color: red</h1>
+          </div>
+        </div>
+      </div>
+      <button onClick={() => exportAsImage(exportRef.current, "test")}>
+        Capture Image
+      </button>
+    </>
+  )
+}
+
+export default App
